9c1a80dc0363ea4191bb9c929c7dc352bbb9e389
Ney2 <kidistdinku5057@gmail.com>
2024-06-24 13:31:47 +0300

    Add solution to maximum number of k-sum paris

---
+++ MaximumNumberOfK-SumPairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length - 1, count = 0;
+        while(i < j) {
+            if(nums[i] + nums[j] > k) {
+                j--;
+            }
+            else if(nums[i] + nums[j] < k) {
+                i++;
+            }
+            else {
+                count++;
+                i++;
+                j--;
+            }
+        }
+
+        return count;
+    }
+}
